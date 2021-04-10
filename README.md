2d12a515b36a9297c19996e5b01ca90a60839c28
Luca Tumedei <luca@theAverageDev.com>
2021-04-10 04:06:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Windows dotfiles
+
+A collection of Windows dotfiles.
+
+## Requirements and installation
+All binaries are, for the most, PowerShell v3+ scripts.
+The repository root directory should be cloned to a binaries directory in the Windows $PATH env var.
+I have them in the `C:\tools\bin` directory.
---
+++ xoff.ps1
@@ -0,0 +1,17 @@
+# Gets hold of the location of the system-wide PHP installation, and disables the XDebug
+# extension by uncommenting its extension loading line.
+
+# Get hold of the PHP php.ini file.
+$phpIniFullOutput = (php --ini) | Select-String -Pattern 'Loaded Configuration File:'
+$phpIniFilePath = ($phpIniFullOutput -split 'Loaded Configuration File:')[1].Trim()
+
+if([string]::IsNullOrWhiteSpacE($phpIniFilePath)){
+	Write-Host "php.ini file could not be found."
+	exit 1
+}
+
+# Replace in place.
+(Get-Content -Path $phpIniFilePath) -replace '^(zend_extension\s?=.*xdebug.*.dll$)', ';$1' | Set-Content $phpIniFilePath
+
+# Display the current PHP version information to debug the effect of the command.
+php -v
